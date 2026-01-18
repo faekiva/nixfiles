f44a9457e391fc610db71ac355796f9fe7066eab
kiva <git@kiva.lgbt>
2026-01-18 13:52:29 -0600

    fix

--- config/hosts/sachi/configuration.nix
+++ config/hosts/sachi/configuration.nix
@@ -122,7 +122,6 @@
   environment.systemPackages = with pkgs; [
      micro
      git
-     kvm
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
