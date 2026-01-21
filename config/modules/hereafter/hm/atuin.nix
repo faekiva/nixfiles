39400be7f53893e7f4afdeddab3fa35a5f5911ff
kiva <git@kiva.lgbt>
2026-01-21 12:09:34 -0600

    ;

--- config/modules/hereafter/hm/atuin.nix
+++ config/modules/hereafter/hm/atuin.nix
@@ -13,10 +13,10 @@
       sync_frequency = "5m";
       sync_address = "https://api.atuin.sh";
       filter_mode_shell_up_key_binding = "directory";
-      style = "compact"
-      inline_height = 20
-      enter_accept = true
-      sync.records = true
+      style = "compact";
+      inline_height = 20;
+      enter_accept = true;
+      sync.records = true;
       search_mode = "fuzzy";
     };
   };
