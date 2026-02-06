1d02c40ca94b40ed69512db1c7f0586c54a8e938
Kiva Hilgenberg <khilgenberg@rewardsnetwork.com>
2026-02-06 12:05:59 -0600

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 cd ./config
 
 if [[ "$(uname)" == "Darwin" ]]; then
     if command -v darwin-rebuild &>/dev/null; then
         sudo darwin-rebuild switch --flake ".#$NIXFILES_HOSTNAME"
     else
-        STATE_VERSION=$(grep 'system\.stateVersion' "config/hosts/Darwin/$NIXFILES_HOSTNAME/configuration.nix" | sed 's/.*=\s*\([0-9]*\).*/\1/')
+        STATE_VERSION=$(grep 'system\.stateVersion' "./hosts/Darwin/$NIXFILES_HOSTNAME/configuration.nix" | sed 's/.*=\s*\([0-9]*\).*/\1/')
         sudo nix run "nix-darwin/nix-darwin-$STATE_VERSION#darwin-rebuild" -- switch --flake ".#$NIXFILES_HOSTNAME"
     fi
 else
-    sudo nixos-rebuild switch --flake "./config#$NIXFILES_HOSTNAME"
+    sudo nixos-rebuild switch --flake ".#$NIXFILES_HOSTNAME"
 fi
