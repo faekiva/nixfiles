01cf28c9f0ac3159bce8b58331b33575a66e8739
kiva <git@kiva.lgbt>
2026-02-05 23:01:23 -0600

    Deploy

--- config/hosts/Darwin/batgirl/kiva-home.nix
+++ config/hosts/Darwin/batgirl/kiva-home.nix
@@ -5,7 +5,10 @@
 }:
 {
   home.stateVersion = "25.11";
-  imports = [ "${flakeRoot}/modules/hereafter/hm/level1-packages.nix" ];
+  imports = [ 
+    "${flakeRoot}/modules/hereafter/hm/level1-packages.nix" 
+    "${flakeRoot}/modules/hereafter/hm/ai-packages.nix"
+  ];
   home.packages = [
     pkgs.nickel
     pkgs.nls
---
+++ config/modules/hereafter/hm/ai-packages.nix
@@ -0,0 +1,9 @@
+{
+  pkgs,
+  ...
+}:
+{
+  home.packages = [ 
+    pkgs.aider-chat
+  ];
+}
