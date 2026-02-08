b715d07d56aef14071d84b9d49fad2ad1daf3f10
kiva <git@kiva.lgbt>
2026-02-07 19:46:14 -0600

    Deploy

--- config/modules/hereafter/hm/direnv.nix
+++ config/modules/hereafter/hm/direnv.nix
@@ -9,11 +9,11 @@
   programs = {
     direnv = {
       enable = true;
-      enableBashIntegration = true; # see note on other shells below
-      enableZshIntegration = true;
+      # enableBashIntegration = true; # see note on other shells below
+      # enableZshIntegration = true;
       nix-direnv.enable = true;
     };
-    bash.enable = true; # see note on other shells below
-    zsh.enable = true;
+    # bash.enable = true; # see note on other shells below
+    # zsh.enable = true;
   };
 }
