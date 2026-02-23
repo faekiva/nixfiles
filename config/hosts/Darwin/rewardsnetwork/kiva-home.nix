1f8e9d2fcf9a91d0e11d0bf1d273941e89dccafc
Kiva Hilgenberg <khilgenberg@rewardsnetwork.com>
2026-02-23 12:40:44 -0600

    apply current state

--- config/hosts/Darwin/rewardsnetwork/kiva-home.nix
+++ config/hosts/Darwin/rewardsnetwork/kiva-home.nix
@@ -1,12 +1,31 @@
 {
+  config,
   inputs,
+  lib,
+  pkgs,
   ...
 }:
+let
+  hasPackage = pkg: builtins.elem pkg config.home.packages;
+  hasAstGrep = hasPackage pkgs.ast-grep;
+  hasClaude = hasPackage pkgs.claude-code;
+in
 {
   home.stateVersion = "25.11";
   imports = [
     "${inputs.flakeRoot}/modules/hereafter/hm/level1-packages.nix"
     "${inputs.flakeRoot}/modules/hereafter/hm/ai-packages.nix"
   ];
-  home.packages = [];
+  home.packages = [
+  	pkgs.nickel
+  	pkgs.ast-grep
+  ];
+  home.file.".claude/skills/ast-grep-agent-skill" = lib.mkIf (hasAstGrep && hasClaude) {
+    source = pkgs.fetchFromGitHub {
+      owner = "ast-grep";
+      repo = "agent-skill";
+      rev = "577f4d4507678f2c8cee150fae25e6ce309f70b1";
+      hash = "sha256-LgGFtPieyKtoru22AhHW8hvkJ8kCHO2Cr8rBOWGuxvY=";
+    };
+  };
 }
