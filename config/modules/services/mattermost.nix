eeb71fafa3e921fbaf774e227bd4b2524404cea2
kiva <git@kiva.lgbt>
2026-02-25 19:38:17 -0600

    mattermost changes

--- config/modules/services/mattermost.nix
+++ config/modules/services/mattermost.nix
@@ -7,10 +7,29 @@
 
   networking.firewall.allowedTCPPorts = [ 8065 ];
 
-  services.mattermost = {
-    enable = true;
-    siteUrl = "https://mattermost.kiva.lgbt";
-    host = "0.0.0.0";
-    port = 8065;
+  containers.mattermost = {
+    config =
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+      {
+        services.mattermost = {
+          enable = true;
+          siteUrl = "https://mattermost.kiva.lgbt";
+          host = "0.0.0.0";
+          port = 8065;
+        };
+
+        system.stateVersion = 25.11;
+        networking = {
+          firewall = {
+            enable = true;
+            allowedTCPPorts = [ 8065 ];
+          };
+        };
+      };
   };
 }
