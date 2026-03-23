48d767dd46f1f5c1d09ced557ae542767cb1ec01
Kiva Hilgenberg <khilgenberg@rewardsnetwork.com>
2026-03-23 13:25:33 -0500

    ast-grep claude

--- config/hosts/Darwin/rewardsnetwork/kiva-home.nix
+++ config/hosts/Darwin/rewardsnetwork/kiva-home.nix
@@ -20,12 +20,14 @@ in
   	pkgs.nickel
   	pkgs.ast-grep
   ];
-  home.file.".claude/skills/ast-grep-agent-skill" = lib.mkIf (hasAstGrep && hasClaude) {
-    source = pkgs.fetchFromGitHub {
-      owner = "ast-grep";
-      repo = "agent-skill";
-      rev = "577f4d4507678f2c8cee150fae25e6ce309f70b1";
-      hash = "sha256-LgGFtPieyKtoru22AhHW8hvkJ8kCHO2Cr8rBOWGuxvY=";
-    };
+  home.file.".claude/skills/ast-grep" = lib.mkIf (hasAstGrep && hasClaude) {
+    source =
+      pkgs.fetchFromGitHub {
+        owner = "ast-grep";
+        repo = "agent-skill";
+        rev = "577f4d4507678f2c8cee150fae25e6ce309f70b1";
+        hash = "sha256-LgGFtPieyKtoru22AhHW8hvkJ8kCHO2Cr8rBOWGuxvY=";
+      }
+      + "/ast-grep/skills/ast-grep";
   };
 }
