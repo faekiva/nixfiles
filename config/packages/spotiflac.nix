0086683a87066fcd6d859f2c54adf64ce2cc6658
kiva <git@kiva.lgbt>
2026-05-13 20:50:44 -0500

    spotiflac

--- config/packages/spotiflac.nix
+++ config/packages/spotiflac.nix
@@ -34,14 +34,23 @@ appimageTools.wrapType2 {
   inherit pname version src;
 
   extraInstallCommands = ''
+  # Find the .desktop file (AppImage may name it with different casing)
+  desktop_file=$(find ${appimageContents} -name '*.desktop' -type f | head -1)
+  if [ -z "$desktop_file" ]; then
+    echo "No .desktop file found in AppImage contents"
+    exit 1
+  fi
+  echo "Found desktop file: $desktop_file"
+
   install -Dm644 ${appimageContents}/spotiflac.png \
     $out/share/icons/hicolor/256x256/apps/spotiflac.png
 
-  install -Dm644 ${appimageContents}/spotiflac.desktop \
+  install -Dm644 "$desktop_file" \
     $out/share/applications/spotiflac.desktop
 
-  substituteInPlace $out/share/applications/spotiflac.desktop \
-    --replace-fail 'Exec=SpotiFLAC' 'Exec=spotiflac'
+  # Fix Exec line and remove TryExec (which hides the app from menus when the path doesn't exist)
+  sed -i 's/^Exec=.*/Exec=spotiflac/' $out/share/applications/spotiflac.desktop
+  sed -i '/^TryExec=/d' $out/share/applications/spotiflac.desktop
 '';
 
   extraPkgs = pkgs: runtimeLibs ++ gstPlugins;
