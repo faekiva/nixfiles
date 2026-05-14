7ab18bfb1eecbbca55e49efa8b59ea5f9551408c
kiva@sachi <git+sachi@kiva.lgbt>
2026-05-13 20:39:21 -0500

    spotiflac

--- config/packages/spotiflac.nix
+++ config/packages/spotiflac.nix
@@ -13,7 +13,7 @@ let
   version = "7.1.6";
   src = fetchurl {
     url = "https://github.com/spotbye/SpotiFLAC/releases/download/v${version}/SpotiFLAC.AppImage";
-    hash = "1zd8sb1gmcbiy3a472a6as8fb2r7vszx0qvgzksqc7fxbb7w6axg";
+    hash = "sha256-1zd8sb1gmcbiy3a472a6as8fb2r7vszx0qvgzksqc7fxbb7w6axg";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
